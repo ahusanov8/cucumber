ead0cf2e42df0d356e62540de93c7307eaeb07c7
ahusanov <asotoolsny@gmail.com>
2019-12-10 17:53:29 -0500

    initial commit of draft project

---
+++ src/test/java/stepDefinition/LoginStepDefinition.java
@@ -0,0 +1,11 @@
+package stepDefinition;
+
+public class LoginStepDefinition {
+//
+//    @Given("User is on Net Banking landing page")
+//    public void user_is_on_NetBanking_landing() {
+//
+//    }
+}
+
+
