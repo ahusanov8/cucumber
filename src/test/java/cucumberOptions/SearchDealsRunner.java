ebd9dc071c29ca0b174a02727f419ebba6b0d477
ahusanov <asotoolsny@gmail.com>
2019-12-11 00:14:09 -0500

    successfull execution of runner files, fixing passing parameters in gherkin

---
+++ src/test/java/cucumberOptions/SearchDealsRunner.java
@@ -0,0 +1,14 @@
+package cucumberOptions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/java/features",
+        glue = "stepDefinitions"
+)
+public class SearchDealsRunner {
+}
--- src/test/java/stepDefinitions/SearchDealsStepDefinition.java
+++ src/test/java/stepDefinitions/SearchDealsStepDefinition.java
@@ -1,40 +1,40 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
+import io.cucumber.java.en.When;
 
-@RunWith(Cucumber.class)
 public class SearchDealsStepDefinition {
 
     @Given("user is on Amazon Home page")
-    public void userIsOnAmazonHomePage() {
+    public void user_is_on_amazon_home_page() {
         // TODO: code to initialize browser
         // navigate to amazon.com
         System.out.println("userIsOnAmazonHomePage");
     }
-    @When("user selects 'Deals' option from Departmants")
-    public void userSelectsOptionFromDepartments(String option) {
+
+    @When("user selects {string} option from Departmants")
+    public void user_selects_option_from_Departmants(String option) {
         // TODO: code here to select an option from drop down
-        System.out.println("userSelectsOptionFromDepartments");
+        System.out.println("userSelectsOptionFromDepartments: " + option);
     }
 
     @When("user clicks on search button")
-    public void userClicksOnSearchButton() {
+    public void user_clicks_on_search_button() {
         //Todo: code to click the seach button
         System.out.println("userClicksOnSearchButton");
     }
+
     @Then("search displays the result set")
-    public void searchDisplaysTheResultSet() {
+    public void search_displays_the_result_set() {
         //todo: code to verify result count > 0
         System.out.println("searchDisplaysTheResultSet");
     }
-    @Then("result container header should have 'Shop all deals'")
-    public void resultContainerHeaderShoudHaveValue(String message) {
+
+    @Then("result container header should have {string}")
+    public void result_container_header_should_have_message(String message) {
         //todo: code to verify the message
-        System.out.println("resultContainerHeaderShoudHaveValue");
+        System.out.println("resultContainerHeaderShoudHaveValue:  " + message);
     }
 
 
