774c19b238dda95397f949fe0612a38f289b5eec
ahusanov <asotoolsny@gmail.com>
2019-12-10 19:05:08 -0500

    Feature file and step definitions updated

---
+++ src/test/java/features/SearchDeals.feature
@@ -0,0 +1,8 @@
+Feature: Amazon Deals Search without Login
+
+Scenario: Verify Amazon Home page defualt menu bar
+Given user is on Amazon Home page
+When user selects 'Deals' option from Departmants
+When user clicks on search button
+Then search displays the result set
+Then result container header should have 'Shop all deals'
---
+++ src/test/java/stepDefinitions/SearchDealsStepDefinition.java
@@ -0,0 +1,39 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class SearchDealsStepDefinition {
+
+    @Given("user is on Amazon Home page")
+    public void userIsOnAmazonHomePage() {
+        // TODO: code to initialize browser
+        // navigate to amazon.com
+        System.out.println("userIsOnAmazonHomePage");
+    }
+    @When("user selects 'Deals' option from Departmants")
+    public void userSelectsOptionFromDepartments(String option) {
+        // TODO: code here to select an option from drop down
+        System.out.println("userSelectsOptionFromDepartments");
+    }
+
+    @When("user clicks on search button")
+    public void userClicksOnSearchButton() {
+        //Todo: code to click the seach button
+        System.out.println("userClicksOnSearchButton");
+    }
+    @Then("search displays the result set")
+    public void searchDisplaysTheResultSet() {
+        //todo: code to verify result count > 0
+        System.out.println("searchDisplaysTheResultSet");
+    }
+    @Then("result container header should have 'Shop all deals'")
+    public void resultContainerHeaderShoudHaveValue(Sting message) {
+        //todo: code to verify the message
+        System.out.println("resultContainerHeaderShoudHaveValue");
+    }
+
+}
+
+
